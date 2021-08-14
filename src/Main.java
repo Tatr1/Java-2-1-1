c96d3d4e07db55df89f29460e475f451d371939f
Tatr1 <ttttt_68@mail.ru>
2021-08-15 01:16:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 115;
+
+        int priceForMile = 20;
+        int mile = price / priceForMile;
+        System.out.println("Начислено бонусных миль: " + mile);
+    }
+}
